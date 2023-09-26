38fba700ad536a99ebca1118d4f6625240f4a59a
Andre <andre.milano09@gmail.com>
2023-09-26 19:43:52 +0700

    update

---
+++ part 7/func1.js
@@ -0,0 +1,15 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!",
+// yang kemudian akan ditampilkan di console.
+
+//bikin fungsinya disini
+
+function shoutOut() {
+  let hasil = "Halo Function!";
+  return hasil;
+}
+console.log(shoutOut());
+
+// ------------------------------------
+console.log("");
+// ------------------------------------
---
+++ part 7/func2.js
@@ -0,0 +1,17 @@
+// Tugas 2
+// Buatlah sebuah fungsi bernama calculateMultiply(), yang mengembalikan nilai berupa hasil kali dari
+// dua parameter yang dikirim.
+
+//bikin fungsinya disini
+
+function calculateMultiply(num1, num2) {
+  let hasil = num1 * num2;
+  return hasil;
+}
+
+hasilPerkalian = calculateMultiply(2, 3);
+console.log(`hasilnya adalah ${hasilPerkalian}`);
+
+// ------------------------------------
+console.log("");
+// -----------------------------------
