60e5d4087479ec66604b85582f43bafbcd5d5bb3
Andre <andre.milano09@gmail.com>
2023-09-26 18:43:05 +0700

    update

--- part 5.5/quest4.js
+++ part 5.5/quest4.js
@@ -8,7 +8,7 @@
 // let firstWordLength = exampleFirstWord4.length;
 // console.log('First Word: ' + exampleFirstWord + ', dengan length: ' + firstWordLength);
 
-let word4 = 'wow JavaScript is so cool'
+let word4 = "wow JavaScript is so cool";
 
 let first_word4 = word4.substring(0, 3);
 let first_word_length = first_word4.length;
@@ -22,11 +22,15 @@ let third_word_length = third_word4.length;
 let fourth_word4 = word4.substring(18, 20);
 let fourth_word_length = fourth_word4.length;
 
-let fifth_word4 = word4.substring (21);
+let fifth_word4 = word4.substring(21);
 let fifth_word_length = fifth_word4.length;
 
 console.log(`First Word: "${first_word4}" dengan length: ${first_word_length}`);
-console.log(`Second Word: "${second_word4}" dengan length: ${second_word_length}`);
+console.log(
+  `Second Word: "${second_word4}" dengan length: ${second_word_length}`
+);
 console.log(`Third Word: "${third_word4}" dengan length: ${third_word_length}`);
-console.log(`Fourth Word: "${fourth_word4}" dengan length: ${fourth_word_length}`);
+console.log(
+  `Fourth Word: "${fourth_word4}" dengan length: ${fourth_word_length}`
+);
 console.log(`Fifth Word: "${fifth_word4}" dengan length: ${fifth_word_length}`);
---
+++ part 6.5/looping.js
@@ -0,0 +1,7 @@
+let name = "agus";
+let result = "";
+
+for (let i = name.length - 1; i >= 0; i--) {
+  result += name[i];
+}
+console.log(result);
